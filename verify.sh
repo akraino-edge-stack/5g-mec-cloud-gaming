9b53063de881b8f91f6ed92bfdf6a5276680cc1c
feng <fengjiaxin22@163.com>
2020-05-13 11:18:03 +0800

    add submodule 5GCEmulator
    Change-Id: Ie77cce0384efcafe39a6eeffde69c12cf137e417
    
    add submodule 5GCEmulator
    add verify file for 5GCEmulator
    
    Change-Id: I5fa60bb33ff8088e7ed0fe79fe7ebe78fd1a2c73

---
+++ verify.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "5g-mec-cloud-gaming verify test"
+
+if [ -z "${GO_URL}" ]; then
+    GO_URL='https://dl.google.com/go/'
+fi
+
+if [ -z "${GO_VERSION}" ]; then
+    GO_VERSION='go1.13.4.linux-amd64.tar.gz'
+fi
+
+set -e -u -x -o pipefail
+
+echo "---> Installing golang from ${GO_URL} with version ${GO_VERSION}"
+
+# install go
+wget ${GO_URL}/${GO_VERSION}
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf ${GO_VERSION}
+
+ls /usr/local
+export PATH=$PATH:/usr/local/go/bin/
+export PATH=$PATH:/usr/bin/
+
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega/
+
+git submodule update --init --recurse
+make -C 5GCEmulator/ngc build
