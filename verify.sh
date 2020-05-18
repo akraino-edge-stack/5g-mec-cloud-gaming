a7cddbd127204bb69a7628f5f0b86915cacdb490
ucasxuxm <xuxm@dsp.ac.cn>
2020-05-18 17:51:04 +0800

    add docker-compose and make three submodule
    Signed-off-by: ucasxuxm <xuxm@dsp.ac.cn>
    Change-Id: I274a74bbe80ac15d7fd430e105692ac21e9fde83

--- verify.sh
+++ verify.sh
@@ -2,37 +2,87 @@
 
 echo "5g-mec-cloud-gaming verify test"
 
+DIR=$(cd `dirname $0`;pwd)
+OS=$(facter operatingsystem)
+#install go
 if [ -z "${GO_URL}" ]; then
-    GO_URL='https://dl.google.com/go/'
+  GO_URL='https://dl.google.com/go/'
 fi
 
 if [ -z "${GO_VERSION}" ]; then
-    GO_VERSION='go1.13.4.linux-amd64.tar.gz'
+ GO_VERSION='go1.13.4.linux-amd64.tar.gz'
 fi
 
 set -e -u -x -o pipefail
 
 echo "---> Installing golang from ${GO_URL} with version ${GO_VERSION}"
 
-# install go
 wget ${GO_URL}/${GO_VERSION}
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf ${GO_VERSION}
 
 ls /usr/local
-
-export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin/
 export PATH=$PATH:/usr/bin/
+export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:/usr/local/go/bin
+go version
+#export GOPROXY=https://goproxy.io
 
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega/
+ginkgo version
+
+#install docker
+#curl -sSL https://get.daocloud.io/docker | sh
+
+case "$OS" in
+	Ubuntu)
+     		sudo apt install docker.io
+   		 ;;
+    	CentOS|RedHat)
+       		sudo yum install -y yum-utils  device-mapper-persistent-data  lvm2
+
+		sudo yum-config-manager    --add-repo   https://download.docker.com/linux/centos/docker-ce.repo
+
+		sudo yum install -y  docker-ce docker-ce-cli containerd.io 
+		 ;;
+esac
+docker version
+
+sudo service docker start
+sudo mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://registry.docker-cn.com","http://hub-mirror.c.163.com"]
+}
+EOF
+sudo systemctl restart docker
+
+#install docker-compose
+case "$OS" in
+   	 Ubuntu)
+   		 ;;
+    	CentOS|RedHat)
+       		sudo yum -y install epel-release
+		sudo yum install python-pip
+   		 ;;
+esac
+cd /usr/local/bin/
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` >  /usr/local/bin/docker-compose
+
+rename docker-compose-Linux-x86_64 docker-compose docker-compose-Linux-x86_64
+chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+mkdir $GOPATH/src/go.ectd.io
+cd  $GOPATH/src/go.ectd.io/
+git clone https://github.com/etcd-io/bbolt.git
 
 git submodule update --init --recursive
+
+cd $DIR
 make -C 5GCEmulator/ngc build
-#echo `ginkgo version`
-#ginkgo version
-#make -C 5GCEmulator/ngc nef
-#ginkgo -v -cover ./pkg/nef
 sudo --preserve-env=PATH make -C 5GCEmulator/ngc test-unit-nef
+make -C edgenode networkedge
+make -C edgecontroller build-dnscli &&  make -C edgecontroller test-dnscli
